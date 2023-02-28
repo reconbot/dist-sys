7f5258eeeed850bca27c7537dfe3092a6e3547fa
Francis Gulotta <wizard@roborooter.com>
2023-02-28 18:58:05 -0500

    perfect replication in a perfect network 3b

--- broadcast/main.go
+++ broadcast/main.go
@@ -10,6 +10,30 @@ import (
 func main() {
 	messages := []int64{}
 	node := maelstrom.NewNode()
+	init := false
+
+	broadcastMessage := func(message int64) {
+		if !init {
+			return
+		}
+		for _, node_id := range node.NodeIDs() {
+			if node_id == node.ID() {
+				return
+			}
+			err := node.Send(node_id, map[string]any{
+				"type":    "broadcast",
+				"message": message,
+			})
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+
+	node.Handle("init", func(msg maelstrom.Message) error {
+		init = true
+		return nil
+	})
 
 	node.Handle("broadcast", func(msg maelstrom.Message) error {
 		var body map[string]any
@@ -20,12 +44,15 @@ func main() {
 		message := int64(body["message"].(float64))
 
 		messages = append(messages, message)
+		broadcastMessage(message)
 
 		response := map[string]any{
 			"type": "broadcast_ok",
 		}
 		return node.Reply(msg, response)
 	})
+
+	node.Handle("broadcast_ok", func(msg maelstrom.Message) error { return nil })
 
 	node.Handle("read", func(msg maelstrom.Message) error {
 		body := map[string]any{
